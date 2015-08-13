4667467c0e9796c3322c41c0cd9fe070b49ac0cf
tranthehuy <tranthehuy.2nd@gmail.com>
2015-08-14 00:39:48 +0700

    c11

--- js/my.js
+++ js/my.js
@@ -4,14 +4,29 @@ app.config(function ($routeProvider){
       .when('/', {
         templateUrl: 'view/main.html',
         controller: 'MainCtrl'
-      });
+      })
+      .when('/board', {
+        templateUrl: 'view/board.html',
+        controller: 'BoardCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'view/about.html',
+        controller: 'AboutCtrl'
+      })
+      ;
 });
 
 app.controller('MainCtrl',function($scope){
-	
 	$scope.title='13 CARDS GAME';
 	$scope.start='Start now!';
 	$scope.about='About me ^^';
 	$scope.footer='Johnny Chen copyright 2015';
+});
+
+app.controller('BoardCtrl',function($scope){
+
+});
+
+app.controller('AboutCtrl',function($scope){
 
 });
